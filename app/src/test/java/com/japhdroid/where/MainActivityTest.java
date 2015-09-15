b94760d7cec5d37a62baa63207718bc31e9841eb
JP Hashagen <jayp33e@gmail.com>
2015-09-16 00:18:25 +0200

    Test empty ListView in MainActivity contains placeholder

--- app/src/test/java/com/japhdroid/where/MainActivityTest.java
+++ app/src/test/java/com/japhdroid/where/MainActivityTest.java
@@ -2,11 +2,16 @@ package com.japhdroid.where;
 
 import android.widget.ListView;
 
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,11 +21,27 @@ import static org.junit.Assert.assertEquals;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class MainActivityTest {
+
+    ActivityController<MainActivity> mController;
+
+    @Before
+    public void setUp() throws Exception {
+        mController = Robolectric.buildActivity(MainActivity.class);
+    }
+
     @Test
-    public void testEmptyListViewContainsPlaceholder() throws Exception {
-        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+    public void testEmptyCatalogListViewContainsPlaceholder() throws Exception {
+        MainActivity activity = mController.create().start().resume().visible().get();
         ListView list = (ListView) activity.findViewById(R.id.list);
-        assertEquals(list.getCount(), 1);
-        assertEquals(list.getItemAtPosition(0).toString(), activity.getString(R.string.message_no_catalog));
+        RuntimeExceptionDao<CatalogTable, Integer> catalogDao = activity.getHelper().getCatalogTableDao();
+        if (!DataProvider.ExistCatalogs(catalogDao)) {
+            assertEquals(list.getCount(), 1);
+            assertEquals(list.getItemAtPosition(0).toString(), activity.getString(R.string.message_no_catalog));
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mController = mController.pause().stop().destroy();
     }
 }
